cf8d811bb5e759e7558d00e0fa3826a284a78ecb
Sam <samuel_morgan_@outlook.com>
2020-06-11 16:30:55 +0100

    Made css folder

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import App from './components/App';
+
+import './css/index.css';
+import './css/app.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
