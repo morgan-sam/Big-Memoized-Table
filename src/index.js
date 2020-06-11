64d1ef2063e3c84f4ae380ed3ae987a23619d267
Sam <samuel_morgan_@outlook.com>
2020-06-11 16:34:10 +0100

    Absolute imports

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App';
+import App from 'components/App';
 
-import './css/index.css';
-import './css/app.css';
+import 'css/index.css';
+import 'css/app.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
