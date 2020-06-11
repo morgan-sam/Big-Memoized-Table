d516de3f795736c72ed5f52169c5c7b9803cf2d9
Sam <samuel_morgan_@outlook.com>
2020-06-11 17:03:16 +0100

    getUsers fn

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,14 @@ import React from 'react';
 import Table from 'components/Table';
 
 function App() {
+	const getUsers = async (num) => {
+		const raw = await fetch(`https://randomuser.me/api/?results=${num}`);
+		const json = await raw.json();
+		const data = json.results;
+		console.log(data);
+		return data;
+	};
+
 	return (
 		<div className="App">
 			<Table />
