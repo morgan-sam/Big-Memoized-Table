7d4db6de4e4946dbe059264931b900d7398e508e
Sam <samuel_morgan_@outlook.com>
2020-06-11 16:29:22 +0100

    Demo table component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import './App.css';
+import Table from './components/Table';
 
 function App() {
-	return <div className="App">App</div>;
+	return (
+		<div className="App">
+			<Table />
+		</div>
+	);
 }
 
 export default App;
