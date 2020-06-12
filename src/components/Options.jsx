fd53280d8fc7cd62f27b897a62f00558aa9b3a44
Sam <samuel_morgan_@outlook.com>
2020-06-12 19:28:50 +0100

    Renamed to setUserCountResetConsole

--- src/components/Options.jsx
+++ src/components/Options.jsx
@@ -4,7 +4,7 @@ const Options = (props) => {
 	const { data, setUsers, memoization, setMemoization, screenConsole, setScreenConsole, setMessages } = props;
 
 	const infoStyle = { display: 'flex', alignItems: 'center', border: '1px solid black', padding: '1rem' };
-	const setUserCountResetMessages = (num) => {
+	const setUserCountResetConsole = (num) => {
 		setUsers(num);
 		setMessages([]);
 	};
@@ -23,9 +23,9 @@ const Options = (props) => {
 			>
 				Turn Memoization {memoization ? 'Off' : 'On'}
 			</button>
-			<button onClick={() => setUserCountResetMessages(20)}>20 Users</button>
-			<button onClick={() => setUserCountResetMessages(100)}>100 Users</button>
-			<button onClick={() => setUserCountResetMessages(1000)}>1000 Users</button>
+			<button onClick={() => setUserCountResetConsole(20)}>20 Users</button>
+			<button onClick={() => setUserCountResetConsole(100)}>100 Users</button>
+			<button onClick={() => setUserCountResetConsole(1000)}>1000 Users</button>
 			<button onClick={() => setScreenConsole(!screenConsole)}>
 				Use {screenConsole ? 'Off' : 'On'}screen Console
 			</button>
