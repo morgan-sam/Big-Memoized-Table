8aa8f4fe9dd0c5c8d0f8e628ebae2f7461ffe2b1
Sam <samuel_morgan_@outlook.com>
2020-06-13 17:45:51 +0100

    Spinner styles sep file

--- src/components/Spinner.jsx
+++ src/components/Spinner.jsx
@@ -1,27 +1,7 @@
 import React from 'react';
+import { loadingStyle, spinnerStyle } from 'style/spinner';
 
-const Spinner = (props) => {
-	const loadingStyle = {
-		display: 'flex',
-		height: '100%',
-		width: '100%',
-		alignItems: 'center',
-		justifyContent: 'center',
-		border: '1px solid black',
-		fontSize: '2rem'
-	};
-
-	const spinnerStyle = {
-		height: '3rem',
-		width: '3rem',
-		borderTop: '10px solid #4287f5',
-		borderRight: '10px solid #eee',
-		borderBottom: '10px solid #eee',
-		borderLeft: '10px solid #eee',
-		borderRadius: '100%',
-		animation: 'spin 1s cubic-bezier(0,-0.04,.07,.82) infinite'
-	};
-
+const Spinner = () => {
 	return (
 		<div style={loadingStyle}>
 			<div style={spinnerStyle} />
---
+++ src/style/spinner.js
@@ -0,0 +1,20 @@
+export const loadingStyle = {
+	display: 'flex',
+	height: '100%',
+	width: '100%',
+	alignItems: 'center',
+	justifyContent: 'center',
+	border: '1px solid black',
+	fontSize: '2rem'
+};
+
+export const spinnerStyle = {
+	height: '3rem',
+	width: '3rem',
+	borderTop: '10px solid #4287f5',
+	borderRight: '10px solid #eee',
+	borderBottom: '10px solid #eee',
+	borderLeft: '10px solid #eee',
+	borderRadius: '100%',
+	animation: 'spin 1s cubic-bezier(0,-0.04,.07,.82) infinite'
+};
