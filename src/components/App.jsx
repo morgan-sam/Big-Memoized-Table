55ca2637aedc7c0d71568c9f356b25feb2cc6d0e
Sam <samuel_morgan_@outlook.com>
2020-06-13 10:14:04 +0100

    Removed obsolete import

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, memo } from 'react';
+import React, { useState, useEffect } from 'react';
 import Table from 'components/Table';
 import Options from 'components/Options';
 import Console from 'components/Console';
